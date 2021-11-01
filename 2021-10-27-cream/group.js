21021a3f19337669f084ce3805a3897120b8b926
Dan Octavian <danoctavian91@gmail.com>
2021-11-01 19:04:59 +0000

    add full value calculation

--- 2021-10-27-cream/group.js
+++ 2021-10-27-cream/group.js
@@ -2,13 +2,17 @@ const fs = require('fs');
 const balances = {};
 const balancesPath = __dirname + '/balances';
 
-const rates = require('./cream-pools-rates.json');
-// const rates = Object.keys(rateData).reduce(
-//   (rates, address) => {
-//     return { ...rates, [address.toLowerCase()]: rateData[address].rate };
-//   },
-//   {},
-// );
+const rawRates = require('./cream-pools-rates.json');
+
+const rates = Object.keys(rawRates).reduce(
+  (rates, address) => {
+    return { ...rates, [address.toLowerCase()]: rawRates[address] };
+  },
+  {},
+);
+
+const ethRates = require('./chainlink-eth-rates--block-13499797.json')
+
 
 fs.readdir(balancesPath, function (err, files) {
 
@@ -17,18 +21,31 @@ fs.readdir(balancesPath, function (err, files) {
     return;
   }
 
-  files = files.slice(0, 1)
+  // files = files.slice(0, 1)
 
   files.forEach(file => {
 
     const [, symbol, address] = file.match(/(cr.+)-(0x[a-f0-9]+).json/i);
+    console.log(`${symbol} ${address}`)
+
+    if (!rates[address.toLowerCase()]) {
+      console.error(`Missing rates for ${symbol}. Skipping`);
+      return;
+    }
     const { rate, underlyingDecimals } = rates[address.toLowerCase()];
     const users = require(`${balancesPath}/${file}`);
 
     const affectedAddresses = Object.keys(users).forEach(address => {
       balances[address.toLowerCase()] = balances[address.toLowerCase()] || {};
       const underlyingBalance = users[address] * parseInt(rate) / 10 ** parseInt(underlyingDecimals);
-      balances[address.toLowerCase()][symbol] = underlyingBalance;
+      const underlyingSymbol = symbol.split('cr')[1];
+      if (!ethRates[underlyingSymbol]) {
+        console.error(`Could not find ETH rate for ${underlyingSymbol}. skipping`);
+        return;
+      }
+      const ethRate = ethRates[underlyingSymbol].rate;
+      const valueInEth = underlyingBalance * parseInt(ethRate) / 1e18;
+      balances[address.toLowerCase()][symbol] = valueInEth;
     });
 
   });
